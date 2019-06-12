ee79d07850a1a7701df0740f26bf09fb32b5f32d
Reza Sohbati <R.Sohbati@samenea.com>
2019-06-12 15:14:03 +0430

    add language button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,7 +24,7 @@ import { CircularMenuComponent } from './menu/circular-menu/circular-menu.compon
     MatToolbarModule,
     MatListModule,
     MatTooltipModule,
-    MatSelectModule, 
+    MatSelectModule,  
 	
   ],
   providers: [],
