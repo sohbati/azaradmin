4d62077fa03705db0360e14b6a2116ab2606979e
reza <sohbati@gmail.com>
2019-06-21 10:49:52 +0430

    add

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PagesComponent } from './pages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { PagesRoutingModule } from './pages-routing.module';
+
+
+@NgModule({
+  declarations: [
+    PagesComponent, 
+    DashboardComponent],
+  imports: [
+    CommonModule,
+    PagesRoutingModule,
+      ]
+})
+export class PagesModule {
+  
+ }
